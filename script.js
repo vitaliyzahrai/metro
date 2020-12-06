506c25bf348744eb27bd9cdf61dc0a175492f6dc
vitaliyzahrai <vitaliy.zahrai@gmail.com>
2020-12-06 12:15:45 +0200

    slider add

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ $(document).ready(function(){
 		$('body').toggleClass('lock');
 	});
 });
+
+$(document).ready(function() {
+	$('.next').click(function() {
+		var currentSpan = $('.span.curry');
+		var currentSpanIndex = $('.span.curry').index();
+		var nextSpanIndex = currentSpanIndex + 1;
+		var nextSpan = $('.span').eq(nextSpanIndex);
+
+		currentSpan.fadeOut(800);
+		currentSpan.removeClass('curry');
+
+		if (nextSpanIndex == ($('.span:last').index()+ 1)){
+			$('span').eq(0).fadeIn(800);
+			$('span').eq(0).addClass('curry');
+		} else{
+			nextSpan.fadeIn(800);
+			nextSpan.addClass('curry');
+		}
+	});
+
+	$('.prev').click(function(){
+		var currentSpan = $('.span.curry');
+		var currentSpanIndex = $('.span.curry').index();
+		var prevSpanIndex = currentSpanIndex - 1;
+		var prevSpan = $('.span').eq(prevSpanIndex);
+
+		currentSpan.fadeOut(800);
+		currentSpan.removeClass('curry');
+		prevSpan.fadeIn(800);
+		prevSpan.addClass('curry');
+	});
+});
